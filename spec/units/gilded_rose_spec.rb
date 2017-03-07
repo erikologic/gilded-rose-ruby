de68caa1d4b66df33c96768431ed3a3f66ca151b
Enrico Graziani <mrenrich84@gmail.com>
2017-03-07 16:44:15 +0000

    Succesfully tested User Story 3: update_quality it's lowering selling days

--- spec/units/gilded_rose_spec.rb
+++ spec/units/gilded_rose_spec.rb
@@ -1,10 +1,16 @@
 describe GildedRose do
+  let(:item_foo) { double :item, name: 'foo', quality: 5, sell_in: 5 }
+  subject (:gilded_rose) { described_class.new( [item_foo] ) }
+
+  before do
+    allow(item_foo).to receive(:sell_in=)
+    allow(item_foo).to receive(:quality=)
+  end
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    it "lower the sell_in value" do
+      gilded_rose.update_quality
+      expect(item_foo).to have_received(:sell_in=).with(4)
     end
   end
 
