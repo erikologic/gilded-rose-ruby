e43e0c261a25138bb074fe5eb5733c4e39e28f6b
Enrico Graziani <mrenrich84@gmail.com>
2017-03-08 16:42:57 +0000

    Succesfully tested U.S. 4 criteria 4: updating quality degrades quality twice as faster when sell_in = 0
    Had to rearrange the tests to use Struct on Item instead of a double.
    I just realized that it wasn't doing what I intended and that my tests were false positives basically

--- spec/units/gilded_rose_spec.rb
+++ spec/units/gilded_rose_spec.rb
@@ -1,34 +1,38 @@
-describe GildedRose do
-  let(:item) { double :item, name: 'foo', quality: 5, sell_in: 5 }
-  subject (:gilded_rose) { described_class.new( [item] ) }
+ItemStruct = Struct.new(:name, :quality, :sell_in)
 
-  before do
-    allow(item).to receive(:sell_in=)
-    allow(item).to receive(:quality=)
-  end
+describe GildedRose do
+  let(:item) { ItemStruct.new('foo', 5, 5) }
+  subject (:gilded_rose) { described_class.new( [item] ) }
 
   describe "#update_quality" do
     it "lower the sell_in value" do
       gilded_rose.update_quality
-      expect(item).to have_received(:sell_in=).with(4)
+      expect(item.sell_in).to eq(4)
     end
 
     it "lower the quality value" do
       gilded_rose.update_quality
-      expect(item).to have_received(:quality=).with(4)
+      expect(item.quality).to eq(4)
     end
     context "when quality value is 0" do
       it "cannot become negative" do
         item.quality = 0
         gilded_rose.update_quality
-        expect(item).to have_received(:quality=).with(0)
+        expect(item.quality).to eq(0)
       end
     end
     xcontext "when quality value is 50" do
       it "cannot go above 50" do
         item.quality = 50
         gilded_rose.update_quality
-        expect(item).to have_received(:quality=).with(50)
+        expect(item.quality).to eq(50)
+      end
+    end
+    context "when sell date equal 0" do
+      it "degrades twice as faster" do
+        item.sell_in = 0
+        gilded_rose.update_quality
+        expect(item.quality).to eq(3)
       end
     end
   end
