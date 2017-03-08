0f147814c70010c5e4065a904e1558ed1febd74c
Enrico Graziani <mrenrich84@gmail.com>
2017-03-08 16:35:57 +0000

    Written test for U.S. 4 criteria 2: updating quality cannot set value above 50

--- spec/units/gilded_rose_spec.rb
+++ spec/units/gilded_rose_spec.rb
@@ -1,27 +1,34 @@
 describe GildedRose do
-  let(:item_foo) { double :item, name: 'foo', quality: 5, sell_in: 5 }
-  subject (:gilded_rose) { described_class.new( [item_foo] ) }
+  let(:item) { double :item, name: 'foo', quality: 5, sell_in: 5 }
+  subject (:gilded_rose) { described_class.new( [item] ) }
 
   before do
-    allow(item_foo).to receive(:sell_in=)
-    allow(item_foo).to receive(:quality=)
+    allow(item).to receive(:sell_in=)
+    allow(item).to receive(:quality=)
   end
 
   describe "#update_quality" do
     it "lower the sell_in value" do
       gilded_rose.update_quality
-      expect(item_foo).to have_received(:sell_in=).with(4)
+      expect(item).to have_received(:sell_in=).with(4)
     end
 
     it "lower the quality value" do
       gilded_rose.update_quality
-      expect(item_foo).to have_received(:quality=).with(4)
+      expect(item).to have_received(:quality=).with(4)
     end
     context "when quality value is 0" do
       it "cannot become negative" do
-        item_foo.quality = 0
+        item.quality = 0
         gilded_rose.update_quality
-        expect(item_foo).to have_received(:quality=).with(0)
+        expect(item).to have_received(:quality=).with(0)
+      end
+    end
+    xcontext "when quality value is 50" do
+      it "cannot go above 50" do
+        item.quality = 50
+        gilded_rose.update_quality
+        expect(item).to have_received(:quality=).with(50)
       end
     end
   end
