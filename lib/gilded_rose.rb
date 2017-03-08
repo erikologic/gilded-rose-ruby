d5e742c7604684be664e5bd5e47db446b9920fcd
Enrico Graziani <mrenrich84@gmail.com>
2017-03-08 19:49:55 +0000

    quality_update: found a common pattern to modify item.sell_in

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,10 +9,10 @@ class GildedRose
       case item.name
       when "Aged Brie"
         delta_quality = 1
-
-        item.sell_in -= 1
+        delta_sell_in = -1
       when 'Sulfuras, Hand of Ragnaros'
         delta_quality = 0
+        delta_sell_in = 0
       when 'Backstage passes to a TAFKAL80ETC concert'
         delta_quality = case
           when item.sell_in == 0
@@ -25,16 +25,17 @@ class GildedRose
             1
           end
 
-        item.sell_in -= 1
+        delta_sell_in = -1
       else
         delta_quality = item.sell_in > 0 ? -1 : -2
-        item.sell_in -= 1
+        delta_sell_in = -1
       end
 
       item.quality += delta_quality
       item.quality = 0 if item.quality < 0
       item.quality = 50 if item.quality > 50
 
+      item.sell_in += delta_sell_in
     end
   end
 end
